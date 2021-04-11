90d4ff990a9abc5d3636649d759ee6d230ef3a84
shiimaxx <yoshima@shiimaxx.com>
2021-04-11 17:32:17 +0900

    wip

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/shiimaxx/heartilly
 
 go 1.16
 
-require github.com/pelletier/go-toml v1.9.0 // indirect
+require (
+	github.com/pelletier/go-toml v1.9.0 // indirect
+	go.uber.org/zap v1.16.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"math/rand"
+	"net/http"
 	"os"
 	"os/signal"
 	"time"
 
 	toml "github.com/pelletier/go-toml"
+	"go.uber.org/zap"
 )
+
+var logger *zap.Logger
 
 type Config struct {
 	Target []TargetConfig
@@ -20,8 +23,9 @@ type TargetConfig struct {
 }
 
 type Worker struct {
-	URL     string
-	Timeout int
+	URL string
+
+	Client *http.Client
 }
 
 func (w *Worker) run(ctx context.Context) {
@@ -29,12 +33,17 @@ func (w *Worker) run(ctx context.Context) {
 	jitter := rand.Intn(10)
 	time.Sleep(time.Duration(jitter) * time.Second)
 
-	fmt.Printf("start worker: %s\n", w.URL)
+	logger.Info("start worker", zap.String("target", w.URL))
 
 	c := time.Tick(1 * time.Minute)
 	for {
-		fmt.Printf("check: %s\n", w.URL)
-		time.Sleep(time.Second * 10)
+		logger.Info("check", zap.String("target", w.URL))
+
+		if ok, err := w.check(ctx); ok && err == nil {
+			logger.Info("ok")
+		} else {
+			logger.Info("failed")
+		}
 
 		select {
 		case <-c:
@@ -44,7 +53,33 @@ func (w *Worker) run(ctx context.Context) {
 	}
 }
 
+func (w *Worker) check(ctx context.Context) (bool, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, w.URL, nil)
+	if err != nil {
+		return false, err
+	}
+
+	resp, err := w.Client.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return false, nil
+	}
+
+	return true, nil
+}
+
 func main() {
+	l, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	logger = l
+	defer logger.Sync()
+
 	config, err := toml.LoadFile("config.toml")
 	if err != nil {
 		panic(err)
@@ -61,13 +96,19 @@ func main() {
 	defer stop()
 
 	for _, url := range targetURLs {
-		worker := &Worker{URL: url, Timeout: 10}
+		client := http.DefaultClient
+		client.Timeout = 10 * time.Second
+
+		worker := &Worker{
+			URL:    url,
+			Client: client,
+		}
 		go worker.run(ctx)
 	}
 
 	select {
 	case <-ctx.Done():
 		stop()
-		fmt.Println("Interrupt")
+		logger.Info("Interrupt")
 	}
 }
